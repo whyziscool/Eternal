03678d85748fb0f001aa9a4406543d8ca1185699
Ethereal58 <164099979+Ethereal58@users.noreply.github.com>
2024-04-08 18:58:20 -0400

    Update FontEncoder.lua

--- FontEncoder.lua
+++ FontEncoder.lua
@@ -13,15 +13,3 @@ for Index, FallenGuardActor in next, getactors() do
         end);
     ]])
 end;
-
-local Stats = game:GetService("Stats");
-
-local Namecall; Namecall = hookmetamethod(Stats, "__namecall", function(self, ...)
-    local Method = getnamecallmethod();
-
-    if Method == "GetTotalMemoryUsageMb" then
-        return 0/0;
-    end;
-
-    return Namecall(self, ...)
-end);
