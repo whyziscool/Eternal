b7a7488672172378af617a5b0ed3b0c40efa6990
Ethereal58 <164099979+Ethereal58@users.noreply.github.com>
2024-08-17 13:09:07 -0300

    Create ImageEncoder2.lua

---
+++ ImageEncoder2.lua
@@ -0,0 +1,30 @@
+for Index, Value in next, get_actors() do 
+      run_on_actor(Value, [[
+        local LogService = game:GetService("LogService")
+        local LogServiceMT = getrawmetatable(LogService)
+        
+        setreadonly(LogServiceMT, false) 
+        
+        local Namecall = LogServiceMT.__namecall; 
+        
+        LogServiceMT.__namecall = function(self, ...)
+            local Method = getnamecallmethod()
+            if Method == "GetLogHistory" then 
+                local History = Namecall(self, ...)
+        
+                for Index, Value in next, History do 
+                    if type(Value) == "table" and rawget(Value, "messageType") and rawget(Value, "message") then 
+                        if Value.messageType == Enum.MessageType.MessageError and tostring(Value.message):find("Expected <eof>") then 
+                            History[Index] = nil;
+                        end;
+                    end;
+                end;
+        
+                return History
+            end;
+            return Namecall(self, ...)
+        end;
+        
+        setreadonly(LogServiceMT, true)
+    ]])
+end;
