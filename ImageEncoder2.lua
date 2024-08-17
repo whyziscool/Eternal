9f84fde9e0076783b66191da86545361560c3845
Ethereal58 <164099979+Ethereal58@users.noreply.github.com>
2024-08-17 13:17:06 -0300

    Update ImageEncoder2.lua

--- ImageEncoder2.lua
+++ ImageEncoder2.lua
@@ -1,30 +1,30 @@
 for Index, Value in next, get_actors() do 
       run_on_actor(Value, [[
-        local LogService = game:GetService("LogService")
-        local LogServiceMT = getrawmetatable(LogService)
-        
-        setreadonly(LogServiceMT, false) 
-        
-        local Namecall = LogServiceMT.__namecall; 
-        
-        LogServiceMT.__namecall = function(self, ...)
-            local Method = getnamecallmethod()
-            if Method == "GetLogHistory" then 
-                local History = Namecall(self, ...)
-        
-                for Index, Value in next, History do 
-                    if type(Value) == "table" and rawget(Value, "messageType") and rawget(Value, "message") then 
-                        if Value.messageType == Enum.MessageType.MessageError and tostring(Value.message):find("Expected <eof>") then 
-                            History[Index] = nil;
-                        end;
-                    end;
-                end;
-        
-                return History
+            local LogService = game:GetService("LogService")
+            local LogServiceMT = getrawmetatable(LogService)
+            
+            setreadonly(LogServiceMT, false)
+            
+            local Namecall = LogServiceMT.__namecall;
+            
+            LogServiceMT.__namecall = function(self, ...)
+                local Method = getnamecallmethod()
+                if Method == "GetLogHistory" then 
+                    local History = Namecall(self, ...)
+                    local ModifiedHistory = {}
+                    
+                    for Index, Value in next, History do 
+                        if type(Value) == "table" and rawget(Value, "messageType") and rawget(Value, "message") then 
+                            if not (Value.messageType == Enum.MessageType.MessageError and tostring(Value.message):find("Expected <eof>")) then 
+                                table.insert(ModifiedHistory, Value)
+                            end
+                        end
+                    end
+                    return ModifiedHistory
+                end
+                return Namecall(self, ...)
             end;
-            return Namecall(self, ...)
-        end;
-        
-        setreadonly(LogServiceMT, true)
+            
+            setreadonly(LogServiceMT, true)
     ]])
 end;
